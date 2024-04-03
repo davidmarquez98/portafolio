f70ecb88ab736121a5b92e65d75973c20dc60468
David Marquez <david.Marquez@outlook.com.ar>
2024-04-03 18:16:55 -0300

    wip

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./../views/page/home/Home.jsx";
 import About from "../views/page/about/About.jsx";
+import Projects from "../views/page/projects/Projects.jsx";
 
 export default function Router() {
   return (
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
+        <Route path="/projects" element={<Projects/>}/>
       </Routes>
   );
 }
