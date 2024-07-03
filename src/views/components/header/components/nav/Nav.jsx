25e048d61076af92768193c17930676034d84aae
davidmarquez98 <davidmarquez2222@outlook.com>
2024-07-03 11:44:48 -0300

    testing nav logic name animation

--- src/views/components/header/components/nav/Nav.jsx
+++ src/views/components/header/components/nav/Nav.jsx
@@ -11,20 +11,27 @@ function Nav(){
     const stringAboutMe = "/portafolio/about";
     const stringProjects = "/portafolio/projects";
 
+    const styleNameSelected = {
+        home: "left: 20px",
+        aboutMe: "left: 0px",
+        projects: "rigth: 20px"
+    };
+
     // let [ nowPage, setNowPage ] = useState("portafolio"); 
     let [ path, setPath ] = useState("");
 
     const validateNavItem = (namePageToValidate) => path == namePageToValidate ? 'nav-item_selected' : '';
 
+    const styleSelected = () => path == stringPortfolio ? styleNameSelected.home : 
+                                path == stringAboutMe ?  styleNameSelected.aboutMe :
+                                path == stringProjects ?  styleNameSelected.projects : '';
 
-    // useEffect( () => { 
-
-    // }, nowPage);
 
     return (
         <div className='nav-container'>
             <LocationWatcher setLocation={setPath}/>
             <div className='nav-content'>
+                <div className="circle-name-selected" style={styleSelected}/>
                 <ul className='nav-lista'>
                     <li className={ validateNavItem(stringPortfolio)}>
                         <Link to="/portafolio"><h3>Home</h3></Link>
