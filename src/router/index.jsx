b668c0ac409c68991f3dda001ed46e2dd5bc16e1
davidmarquez98 <davidarielmarquez@outlook.com>
2024-09-11 20:45:12 -0300

    testing hashrouter

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Router, Routes, Route } from "react-router-dom";
 import Home from "./../views/page/home/Home.jsx";
 import About from "../views/page/about/About.jsx";
 import Projects from "../views/page/projects/Projects.jsx";
@@ -6,13 +6,11 @@ import Contact from "../views/page/contact/Contact.jsx";
 
 export default function AppRouter() {
   return (
-    <Router basename="/portafolio">
-      <Routes>
-        <Route path="/portafolio" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/portafolio" element={<Home/>}/>
+      <Route path="/portafolio/about" element={<About/>}/>
+      <Route path="/portafolio/projects" element={<Projects/>}/>
+      <Route path="/portafolio/contact" element={<Contact/>}/>
+  </Routes>
   );
 }
