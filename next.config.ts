652d04fe13926ef1f394ba9c907df1cbd8c32169
davidamrquez98 <davidarielmarquez@outlook.com>
2026-09-04 16:03:33 -0300

    arquitwctura, diseño terminado, falta actualizar info, funcionalidad de boton para dewscargar cv

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin();
-const nextConfig: NextConfig = {typedRoutes: true};
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  allowedDevOrigins: ['192.168.100.6']
+};
 
 export default withNextIntl(nextConfig);
